14576d371d16dff95c6a55bfc45a5b345be70474
Mohammad Mahdi Bakhshi <bakhshi@protonmail.com>
2023-10-05 21:32:24 +0330

    Set redis client

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const express = require('express');
+import { createClient } from 'redis';
+import express from 'express';
+
+const port = process.env.PORT || '3000'
+const redis_port = process.env.REDIS_PORT || '6379'
+const redis_host = process.env.REDIS_HOST || 'localhost'
+
+const db = await createClient({
+  url: `redis://${redis_host}:${redis_port}`
+}).on('error', err => console.log('Redis Client Error', err))
+  .connect();
 
 const app = express();
 
@@ -16,5 +26,4 @@ app.get('/users/:id', (req, res) => {
   // TODO: return user with :id
 })
 
-const port = process.env.PORT || '3000'
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
